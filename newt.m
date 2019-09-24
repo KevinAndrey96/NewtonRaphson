ee113d0782137bf392642116895ee4c8af332684
Andrey Herrera <kahs_kevin@hotmail.com>
2019-09-23 23:19:24 -0500

    Convertido de do until a while

--- newt.m
+++ newt.m
@@ -2,9 +2,13 @@ function yn=newt(x)
   clc
   format long
   
-  do
+  #do
+   # x=x-(f(x)/df(x));
+  #until (abs(f(x))<=1.0E-10)
+  
+  while 1.0E-10<=(abs(f(x)))
     x=x-(f(x)/df(x));
-  until (abs(f(x))<=1.0E-10)
+  endwhile
   
   fprintf('La raiz es: %20.12f con un error de: %20.12f \n', x,f(x))
   
